482920bf87d123043127b4d8e2ce1d44e5bab03d
Laura - Beatrix Nagy <57393112+laurabeatrixnagy@users.noreply.github.com>
2021-03-10 20:16:39 +0200

    Create README.m

---
+++ polynomial calculator/README.m
@@ -0,0 +1,1 @@
+A polynomial calculator with a dedicated graphical interface through which the user can enter polynomials, select the operation to be performed (i.e. addition, subtraction, multiplication, division, derivative, integration) and display the result.
